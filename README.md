52cf6110cc4a01373c54dd5661b16ba2722a3bfd
Borglin <borglz1@medtronic.com>
2020-12-13 17:20:04 -0800

    Interactive UFO sightings data table complete

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# UFOs
+# **UFO Sighting Reports: An interactive html table**
+Interactive Data Table Application
+- html
+- javascript
+- bootstrap
+
+## **Purpose** 
+This web-based compatible tool displays UFO sighting data for enthusiasts to monitor and filter to study results by the following attributes:
+- **Date of Observation** 
+- **City, State, and Country the Observation was Reported In** 
+- **Shape of the UFO** 
+# 
+The tool is configured to clearly display the information for interested users. Directions for how to use the filters is found below 
+
+## Resources
+- static/data.js
+
+## How to Use Data Filters
+![Unfiltered Data](https://github.com/zborglin/UFOs/blob/main/static/images/unfiltered.png)
+#
+![Filtered Data](https://github.com/zborglin/UFOs/blob/main/static/images/filtered.png)
+
+## Results
+Please reference the images above as you follow the filtering instructions 
+1. The top image shows the unfiltered UFO sightings data table
+- Filter input formats are given by showing a preview value for each filter field
+- All data is shown in the data table as no filters are applied
+2. The bottom image shows the application of a city filter for the data table
+- "el cajon" is entered as a city filter
+- Press enter to apply filter to data table and display the filtered data
+
+## Evaluation of Application
+1. **Drawback of Application Design**: The filter inputs require exact matches in order to return the associated data. If there are any typos or a different format is used, then the results might not show up when the user expects it to.
+2. **Recommendations for Further Development**: In order to increase the quality of this tool, it is recommended that the filter input process be augmented to allow for partial completions and typos to still return the relevant information in the data table to increase ease of use of the tool. In addition, this tool can be improved by adding a web scraping application that automatically pulls new sightings data as they are reported into the table. 
--- static/js/app.js
+++ static/js/app.js
@@ -24,31 +24,31 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-let filters = {};
+var filters = {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    let elem = d3.select(this);
+    let changedElement = d3.select(this);
     
     // 4b. Save the value that was changed as a variable.
-    let elemValue = elem.property("value");
+    let elementValue = changedElement.property("value");
+    console.log(elementValue);
 
     // 4c. Save the id of the filter that was changed as a variable.
-    let elemId = elem.attr("id");
+    let filterId = changedElement.attr("id");
+    console.log(filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-    if (elemValue) {
-      filters.elemId = elemValue
-    } else {
-      filters.delete(elem)
+    if (elementValue) {
+      filters[filterId] = elementValue;
+    } 
+    else {
+      delete filters[filterId]; 
     }
-    
-
-    .delete([varId])
-  
+        
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -56,45 +56,16 @@ function updateFilters() {
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
-  
-    // 8. Set the filtered data to the tableData.
-    filters.datetime = d3.select("#datetime").property("value");
-    filters.city = d3.select("#city").property("value");
-    filters.state = d3.select("#state").property("value");
-    filters.country = d3.select("#country").property("value");
-    filters.shape = d3.select("#shape").property("value");
     
+    // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
+    
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    // forEach
-    
-      if (datetime) {
-        // Apply `filter` to the table data to only keep the
-        // rows where the `datetime` value matches the filter value
-        filteredData = filteredData.filter(row => row.datetime === datetime);  
-      };
-      if (city) {
-        // Apply `filter` to the table data to only keep the
-        // rows where the `datetime` value matches the filter value
-        filteredData = filteredData.filter(row => row.city === city);  
-      };
-      if (state) {
-        // Apply `filter` to the table data to only keep the
-        // rows where the `datetime` value matches the filter value
-        filteredData = filteredData.filter(row => row.state === state);  
-      };
-      if (country) {
-        // Apply `filter` to the table data to only keep the
-        // rows where the `datetime` value matches the filter value
-        filteredData = filteredData.filter(row => row.country === country);  
-      };
-      if (shape) {
-        // Apply `filter` to the table data to only keep the
-        // rows where the `datetime` value matches the filter value
-        filteredData = filteredData.filter(row => row.shape === shape);  
-      };
-  
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   }
